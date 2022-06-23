10f09a0897e8de8c0b70ad14eaa9282bbf33f876
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-06-23 12:39:58 +0900

    feat: app.js (nodejs)

---
+++ app.js
@@ -0,0 +1,15 @@
+const http  = require("http");
+
+const server = http.createServer((req, res) => {
+    if(req.url === "/") {
+        res.write(`<h1>Hello from nodejs</h1>`);
+        console.log("User accessed.")
+    } else {
+        res.write(`<h1>You have entered this url : ${req.url}</h1>`);
+    }
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("The server is listening on port 3000");
+});
